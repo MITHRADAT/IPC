0b60ade84db5b06c362826b50b43eca7e27dcb3c
MITHRADAT <miladlotfimadieh@gmail.com>
2026-09-15 15:19:03 +0330

    shared memory in C#

---
+++ SharedMemory/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string memoryName = "SharedMemory";
+            const long memorySize = 2048;
+            const int messageLen = 4;
+            const int clientOffSet = 1024;
+
+            try
+            {
+
+                using (var memory = MemoryMappedFile.OpenExisting(memoryName, MemoryMappedFileRights.ReadWrite))
+                {
+                    using (var view = memory.CreateViewAccessor(0, memorySize, MemoryMappedFileAccess.ReadWrite))
+                    {
+                        //read from server
+                        var messageLength = new byte[messageLen];
+                        view.ReadArray(0, messageLength, 0, messageLen);
+                        var length = BitConverter.ToInt32(messageLength, 0);
+                        var arr = new byte[length];
+                        view.ReadArray(messageLen, arr, 0, length);
+                        var message = Encoding.UTF8.GetString(arr);
+                        Console.WriteLine($"client received: {message}");
+
+
+                        //send to server
+                        var response = "this is client's response to server";
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        var responseLength = BitConverter.GetBytes(responseBytes.Length);
+                        view.WriteArray(clientOffSet, responseLength, 0, messageLen);
+                        view.WriteArray(clientOffSet + messageLen, responseBytes, 0, responseBytes.Length);
+                        Console.WriteLine($"client sent: {response}");
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"an error occured: {ex}");
+            }
+            finally
+            {
+                Console.WriteLine("the end of client.");
+            }
+        }
+    }
+}
---
+++ SharedMemory/Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string memoryName = "SharedMemory";
+            const long memorySize = 2048;
+            const int messageLen = 4;
+            const int clientOffSet = 1024;
+
+            using (var memory = MemoryMappedFile.CreateOrOpen(memoryName, memorySize, MemoryMappedFileAccess.ReadWrite))
+            {
+                using (var view = memory.CreateViewAccessor(0, memorySize, MemoryMappedFileAccess.ReadWrite))
+                {
+                    //send to client
+                    var message = "this message is from server. i love programming :)";
+                    var arr = Encoding.UTF8.GetBytes(message);
+                    var messageLength = BitConverter.GetBytes(arr.Length);
+                    view.WriteArray(0, messageLength, 0, messageLen);
+                    view.WriteArray(messageLen, arr, 0, arr.Length);
+                    Console.WriteLine($"sever sent: {message}");
+
+                    
+                    //read from client
+                    Thread.Sleep(10000); //this is awful because there shared memory is not a good practice for two way communication because there is no synchronization
+                    var responseLengthBytes = new byte[messageLen];
+                    view.ReadArray(clientOffSet, responseLengthBytes, 0, messageLen);
+                    var responseLength = BitConverter.ToInt32(responseLengthBytes, 0);
+                    var responseBytes = new byte[responseLength];
+                    view.ReadArray(clientOffSet + messageLen, responseBytes, 0, responseLength);
+                    var response = Encoding.UTF8.GetString(responseBytes);
+                    Console.WriteLine($"server received: {response}");
+                    
+                    
+                    
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
