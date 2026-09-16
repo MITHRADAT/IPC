567c2843a9640d750974a0c273caab894283745d
MITHRADAT <miladlotfimadieh@gmail.com>
2026-09-16 15:02:01 +0330

    tcp in C#

---
+++ TransmissionControlProtocol/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int serverPort = 5000;
+
+            using (var socket = new TcpClient())
+            {
+                await socket.ConnectAsync(IPAddress.Loopback, serverPort);
+
+                Console.WriteLine("client connected!");
+
+                using (var stream = socket.GetStream())
+                {
+                    var message = "hi server. Im client. can you explain about tcp?";
+                    var bytes = Encoding.UTF8.GetBytes(message);
+                    await stream.WriteAsync(bytes);
+
+                    var buffer = new byte[1024];
+                    while (true)
+                    {
+                        var bytesRead = await stream.ReadAsync(buffer);
+                        if (bytesRead == 0)
+                        {
+                            break;
+                        }
+
+                        var incomingMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine($"client received: {incomingMessage}");
+                    }
+                    
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TransmissionControlProtocol/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int serverPort = 5000;
+
+            using (var listener = new TcpListener(IPAddress.Loopback, serverPort))
+            {
+                listener.Start();
+                Console.WriteLine("server is listening...");
+
+                using (var socket = await listener.AcceptTcpClientAsync())
+                using (var stream = socket.GetStream())
+                {
+                    Console.WriteLine("client connected!");
+
+                    var buffer = new byte[1024];
+                    var bytesRead = await stream.ReadAsync(buffer);
+                    var incomingMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"server received: {incomingMessage}");
+
+                    var bytes = new[]
+                    {
+                        GetBytes("tcp is transmission control protocol"),
+                        GetBytes("tcp provides a reliable ordered byte stream"),
+                        GetBytes("tcp does not preserve message boundaries"),
+                        GetBytes("tcp has no limitation for size of messages"),
+                        GetBytes("tcp retransmits lost data and guarantees ordered delivery")
+                    };
+
+                    var tasks = bytes.Select(b => stream.WriteAsync(b).AsTask());
+
+                    await Task.WhenAll(tasks);
+
+                    Console.WriteLine("server completed sending stream");
+                    Console.ReadLine();
+                }
+
+                listener.Stop();
+            }
+        }
+
+        private static byte[] GetBytes(string message)
+        {
+            return Encoding.UTF8.GetBytes(message);
+        }
+    }
+}
